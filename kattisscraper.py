f271eda1b4c991b257b1f22a6286e7a3c12f8b64
Thomas Halgaard <31439743+Halgaard@users.noreply.github.com>
2019-09-26 21:34:37 +0200

    Update kattisscraper.py
    Made a very simple CLI interface

--- kattisscraper.py
+++ kattisscraper.py
@@ -1,10 +1,26 @@
 import pyfiglet
 
+
 def printTitleScreen():
     result = pyfiglet.figlet_format("Kattis Scraper")
     print(result)
 
 
 if __name__ == "__main__":
-    PrintTitleScreen()
-    #make CLI loop here!
+    printTitleScreen()
+    print("What do you want to do? (enter help for help)")
+    while True:
+        cliInput = input()
+        cliInput = cliInput.lower()
+
+        # Mega grim løsning.. men switch findes ikke i python så blev lige sådan her sofar
+        # Der findes pænere løsning vi kan lave. Fx noget ala https://data-flair.training/blogs/python-switch-case/
+        if cliInput == 'help':
+            print("The following commands are allowed:")
+            print("help - shows this option")
+            print("title - Reprints the title screen")
+            print("exit - Exits the program")
+        if cliInput == 'title':
+            printTitleScreen()
+        if cliInput == 'exit':
+            break
