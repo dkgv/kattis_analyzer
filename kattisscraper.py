baf1343fdc959cc59c138478c180380223a93e4d
Thomas Halgaard <31439743+Halgaard@users.noreply.github.com>
2019-09-26 17:39:45 +0200

    Create kattisscraper.py
    start of file to CLI tool

---
+++ kattisscraper.py
@@ -0,0 +1,10 @@
+import pyfiglet
+
+def printTitleScreen():
+    result = pyfiglet.figlet_format("Kattis Scraper")
+    print(result)
+
+
+if __name__ == "__main__":
+    PrintTitleScreen()
+    #make CLI loop here!
