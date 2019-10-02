18890c585e200408b977ef810ae2561a39b1a8ab
Thomas Halgaard <31439743+Halgaard@users.noreply.github.com>
2019-10-02 21:26:54 +0200

    added a todo of what the parser should do at the minimum

--- katparser.py
+++ katparser.py
@@ -1,2 +1,14 @@
+"""
+TODO:
+-Parse data from the scraper
+-Perform the same analysis on data as the java equivalent can at the moment
+-It must be possible to define what file to perform parsing on.
+-If such a file is not defined use the latest file sorted by timestamp
+-If there exists no file inform the user to perform af scrape of kattis (or perform it for them)
+-If no flag arguments are given run all tests
+-If flag arguments are present only run the tests corresponding with the flags
+"""
+
+
 def parse():
     print("I AM PARSING!")
